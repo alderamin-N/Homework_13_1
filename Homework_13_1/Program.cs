42c3ac96ddb8ea38c9f89201daa1c1766d77bc29
NPototskaya <kusochek-91@mail.ru>
2023-10-17 20:19:43 +0300

    Add project files.

---
+++ Homework_13_1/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace Homework_13_1
+{
+        internal class Program
+        {
+            static void Main(string[] args)
+            {
+
+                List<int> number = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+                List<int> listNew = new List<int>(number.Top(30));
+
+                foreach (int n in listNew)
+                {
+                    Console.WriteLine(n);
+                }
+
+            }
+        }
+        public static class EnumerableExtentions
+        {
+            public static IEnumerable<T> Top<T>(this IEnumerable<T> list, int number)
+            {
+                if (number < 1 || number > 100)
+                {
+                    throw new ArgumentException("Процент был указан меньше 1 или больше 100");
+                }
+                int count = (int)Math.Ceiling(((list.Count() * (double)number) / 100));
+                return list.OrderByDescending(x => x).Take(count);
+
+            }
+
+            public static IEnumerable<P> Top<T, P>(this IEnumerable<T> list, int number, Func<T, P> condition)
+            {
+                if (number < 1 || number > 100)
+                {
+                    throw new ArgumentException("Процент был указан меньше 1 или больше 100");
+                }
+                int count = (int)Math.Ceiling(((list.Count() * (double)number) / 100));
+                return list.Select(condition).OrderByDescending(x => x).Take(count);
+
+            }
+        }   
+}
+
