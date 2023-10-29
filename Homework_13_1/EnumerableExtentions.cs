f6fdd7f7661f6d95e65759503a3241649eaabe69
NPototskaya <kusochek-91@mail.ru>
2023-10-29 16:07:04 +0300

    исправила

--- Homework_13_1/EnumerableExtentions.cs
+++ Homework_13_1/EnumerableExtentions.cs
@@ -6,28 +6,26 @@ using System.Threading.Tasks;
 
 namespace Homework_13_1
 {
-    public static class EnumerableExtentions
-    {
+    public static class EnumerableExtension
+    {       
         public static IEnumerable<T> Top<T>(this IEnumerable<T> list, int number)
         {
-            if (number < 1 || number > 100)
+            if (number is < 1 or > 100)
             {
                 throw new ArgumentException("Процент был указан меньше 1 или больше 100");
-            }
-            int count = (int)Math.Ceiling(((list.Count() * (double)number) / 100));
+            }            
+            var count = (int)Math.Ceiling(list.Count() * (double)number / 100);
             return list.OrderByDescending(x => x).Take(count);
 
         }
-
         public static IEnumerable<T> Top<T, P>(this IEnumerable<T> list, int number, Func<T, P> condition)
         {
-            if (number < 1 || number > 100)
+            if (number is < 1 or > 100)
             {
                 throw new ArgumentException("Процент был указан меньше 1 или больше 100");
             }
-            int count = (int)Math.Ceiling(((list.Count() * (double)number) / 100));
+            var count = (int)Math.Ceiling(list.Count() * (double)number / 100);
             return list.OrderByDescending(condition).Take(count);
-
         }
     }
 }
